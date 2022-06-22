7d3556dcefaabe68504a879fe313ec3bba388f04
A-3l <aeirizlopez@gmail.com>
2022-06-22 13:51:13 +0200

    Version final sin javadoc

---
+++ src/org/myorg/jguimodule/tree/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+@TemplateRegistrations({
+    
+
+@TemplateRegistration(
+        folder = "Other",
+        iconBase="org/myorg/jguimodule/Archive.jpg",
+        displayName = "#Treetemplate_displayName",
+        content = "Tree.form.template",
+        description = "DescriptionTree.html",
+        scriptEngine="freemarker"),
+    
+@TemplateRegistration(
+        folder = "Other",
+        iconBase="org/myorg/jguimodule/Archive.jpg",
+        displayName = "#Treetemplate_displayName",
+        content = "Tree.java.template",
+        description = "DescriptionTree.html",
+        scriptEngine="freemarker")
+
+})        
+@NbBundle.Messages({"Treetemplate_displayName= JGuiTree file"  }) 
+package org.myorg.jguimodule.tree;
+
+import org.netbeans.api.templates.TemplateRegistration;
+import org.netbeans.api.templates.TemplateRegistrations;
+import org.openide.util.NbBundle;
+
